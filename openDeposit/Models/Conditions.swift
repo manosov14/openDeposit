6389d727e051633aa244f064991a1ca3ecd65f94
manosov14 <59836080+manosov14@users.noreply.github.com>
2021-08-01 10:15:04 +0300

    Добавлен models/Conditions

---
+++ openDeposit/Models/Conditions.swift
@@ -0,0 +1,58 @@
+//
+//  Conditions.swift
+//  openDeposit
+//
+//  Created by Михаил on 01.08.2021.
+//
+
+struct Deposit {
+    static var offer =
+            Offer(
+                offerName: "Вклад накопительный",
+                currency: Currency(
+                    code: 810,
+                    charCode: "Российский рубль"),
+                isWithdrawal: Type(
+                    code: false,
+                    name: "Без снятия"),
+                isReplenishable: Type(
+                    code: false,
+                    name: "Без снятия"),
+                isProlongation: Type(
+                    code: true,
+                    name: "Пролонгируемый"),
+                isCapitalization: Type(
+                    code: true,
+                    name: "Капитализация"),
+                conditions: [
+                    Condition(
+                        sumFrom: 15000,
+                        sumTo: 300000,
+                        interest: 5),
+                    Condition(
+                        sumFrom: 300000,
+                        sumTo: 1000000,
+                        interest: 6),
+                    Condition(
+                        sumFrom: 1000000,
+                        sumTo: 999999999,
+                        interest: 7)
+                ]
+            )
+}
+
+struct Offer {
+    let offerName: String
+    let currency: Currency
+    let isWithdrawal: Type
+    let isReplenishable: Type
+    let isProlongation: Type
+    let isCapitalization: Type
+    let conditions: [Condition]
+}
+
+struct Condition {
+    let sumFrom: Int
+    let sumTo: Int
+    let interest: Int
+}
